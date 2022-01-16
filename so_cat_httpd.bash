8d142d077f46a9055cfbcf79df08007bbb5f4713
JB <djb2@epluribusunix.net>
2022-01-16 17:24:54 -0500

    Delete so_cat_httpd.bash

--- so_cat_httpd.bash
+++
@@ -1,5 +0,0 @@
-##
-## Big thanks to the support of Stack Exchange
-
-# List on non priviledged port 1234, do stuff on localhost / 127.0.0.1
-socat -v -v TCP-LISTEN:1234,crlf,reuseaddr,fork SYSTEM:"echo HTTP/1.0 200; echo Content-Type\: text/plain; echo; echo HelloWorld"
